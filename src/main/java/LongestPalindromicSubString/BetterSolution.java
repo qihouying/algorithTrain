bb04ef298585d8d1f96c7ca6f9959cf9cfe85060
qihouying <dream_0920@qq.com>
2017-10-12 07:16:19 +0800

    leetCode

---
+++ src/main/java/LongestPalindromicSubString/BetterSolution.java
@@ -0,0 +1,10 @@
+package LongestPalindromicSubString;
+
+/**
+ * Created by dream on 2017/10/7.
+ */
+public class BetterSolution {
+    public String longestPalindromicSubString(String s) {
+        return s;
+    }
+}
